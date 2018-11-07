e1fe60b6f484c4ca3ac7c6999f71ba30bd0fc9e2
jasenfinch <jsf9@aber.ac.uk>
2018-11-07 12:42:28 +0000

    added results functions as generic methods

--- R/allGenerics.R
+++ R/allGenerics.R
@@ -70,3 +70,21 @@ setGeneric('plotAlign',function(Experiment, AlignId, per.class=T, xlim=NULL){
 setGeneric('plotChr',function(Experiment, N.sample=1, type=c("BIC","TIC","EIC"), xlim=NULL, mz=NULL){
   standardGeneric('plotChr')
 })
+
+#' @rdname idList
+
+setGeneric('idList',function(object, id.database=mslib){
+  standardGeneric('idList')
+})
+
+#' @rdname alignList
+
+setGeneric('alignList',function(object, by.area=TRUE){
+  standardGeneric('alignList')
+})
+
+#' @rdname dataList
+
+setGeneric('dataList',function(Experiment, id.database=mslib, by.area=TRUE){
+  standardGeneric('dataList')
+})
