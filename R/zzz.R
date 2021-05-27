ad86037fdcecb6f0c59dd6470f934336b82fe96e
jasenfinch <jsf9@aber.ac.uk>
2021-05-27 12:40:29 +0100

    added bug reports URL to DESCRIPTION. Added biocViews field to DESCRIPTION to enable automatic installation of the gcspikelite bioconductor package

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,6 @@
 
 .onAttach <- function(libname, pkgname) {
 
-  data <- try(XML::xmlParse("http://cran.r-project.org/package=erah", isHTML=T), silent=T)
   currVersion <- as.character(utils::packageVersion("erah"))
 
   erah.logo <- paste0("\n
@@ -17,7 +16,6 @@
  - Type 'vignette('eRahManual', package='erah')' for a tutorial on eRah's usage.
  - For bugs, problems and issues, please do not hesitate in contacting xavier.domingoa@eurecat.org or opening an issue on the Github repository (https://github.com/xdomingoal/erah-devel/issues).")
 
-  #msg <- paste("Welcome to eRah. For bugs, problems and issues, please do not hesitate in contacting xdomingo@scripps.edu and describing your problem.", sep="")
   packageStartupMessage(erah.logo)
 }
 
